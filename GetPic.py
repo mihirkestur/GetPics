46a12e4d0541d410d38cb5395f5b7c15bee66a8b
Mihir M Kestur <mihirkestur@outlook.com>
2020-07-05 12:12:15 +0530

    Added size feature and frame rate feature

--- GetPic.py
+++ GetPic.py
@@ -1,13 +1,16 @@
 from PIL import Image
 from PIL import ImageTk
 import tkinter as tk
-import threading,datetime,imutils,cv2,os,time,glob
+import threading,datetime,cv2,os,time,glob
 from tkinter import filedialog
 import numpy as np
 
 class GetPic:
 	def __init__(self):
 		self.cap = cv2.VideoCapture(0)
+		self.root = tk.Tk()
+		self.rate = 1
+		self.size = (300,300)
 		self.pause = False
 		self.piccount = 0
 		self.max_pics = 0
@@ -16,21 +19,30 @@ class GetPic:
 		self.thread = None
 		self.stopEvent = None
 		self.flag = False
-		self.root = tk.Tk()
 		self.panel = None
 		btn_close = tk.Button(self.root, text="Close",command = self.close)
 		btn_close.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
 		btn_submit = tk.Button(self.root, text="Submit",command = self.submit )
 		btn_submit.pack(side="bottom", fill="x", expand="yes", padx=10,pady=10)
+		self.size_text = tk.Text(self.root, height=1, width=5)
+		self.size_text.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
+		self.lbl_size = tk.Label(self.root, text = "Enter the size of the pictures in the form of 'a,b' (in px, default is 300 x 300 px)")
+		self.lbl_size.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
+		self.rate_text = tk.Text(self.root, height=1, width=5)
+		self.rate_text.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
+		self.lbl_rate = tk.Label(self.root, text = "Enter rate at which the pictures should be taken (in seconds, default is 1 picture per second)")
+		self.lbl_rate.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
 		self.max_text = tk.Text(self.root, height=1, width=5)
 		self.max_text.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
+		self.lbl_max_pic = tk.Label(self.root, text = "Enter maximum number of pictures to be taken")
+		self.lbl_max_pic.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
 		btn_start = tk.Button(self.root, text="Start snapshots",command = self.takeSnapshot )
 		btn_start.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
 		btn_save = tk.Button(self.root, text="Save directory",command = self.save_path )
 		btn_save.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
 		btn_pause = tk.Button(self.root, text="Pause/Resume taking pictures",command = self.pause_resume )
 		btn_pause.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
-		self.signal = tk.Label(self.root, text = "Enter number of pictures and choose save directory")
+		self.signal = tk.Label(self.root, text = "Pictures will be taken once start snapshots is pressed")
 		self.signal.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
 		self.stopEvent = threading.Event()
 		self.thread = threading.Thread(target=self.videoLoop, args=())
@@ -42,7 +54,7 @@ class GetPic:
 			while not self.stopEvent.is_set():
 				if self.pause == True: continue
 				_, self.frame = self.cap.read()
-				self.frame = cv2.resize(self.frame, (300,225), interpolation=cv2.INTER_LINEAR)
+				self.frame = cv2.resize(self.frame, self.size, interpolation=cv2.INTER_LINEAR)
 				img = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
 				image = Image.fromarray(img)
 				image = ImageTk.PhotoImage(image)
@@ -55,7 +67,7 @@ class GetPic:
 					self.panel.image = image
 					if self.flag:
 						if (self.piccount != self.max_pics ):
-							time.sleep(0.5)
+							time.sleep(self.rate)
 							filename = "{}.jpg".format(str(self.piccount + 1))
 							path = os.path.sep.join((self.outputpath, filename))
 							cv2.imwrite(path, self.frame)
@@ -73,6 +85,11 @@ class GetPic:
 				self.signal["text"] = "Please enter a positive integer only"
 				return
 			self.max_pics = int(float(self.max_text.get("1.0","end-1c")))
+		if self.size_text.get("1.0","end-1c") != "":
+			self.size = tuple([int(float(i)) for i in (self.size_text.get("1.0","end-1c")).split(',')])
+			print(self.size)
+		if self.rate_text.get("1.0","end-1c") != "":
+			self.rate = float(self.rate_text.get("1.0","end-1c"))
 
 	def takeSnapshot(self):
 		if self.outputpath == "":
